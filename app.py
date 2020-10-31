87222c335c55f7ec1aeae87988c0c794cbf3b379
collinsculley <collinsculley@yahoo.com>
2020-10-31 13:01:49 -0500

    folder structure update for Heroku

--- app.py
+++ app.py
@@ -1,28 +1,18 @@
-#  Example Structure From Module 10
-
-
+# Flask App
 
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
-import scraping
 
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
-mongo = PyMongo(app)
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+# mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-    mars = mongo.db.mars.find_one()
-    return render_template("index.html", mars=mars)
-
-@app.route("/scrape")
-def scrape():
-    mars = mongo.db.mars
-    mars_data = scraping.scrape_all()
-    mars.update({}, mars_data, upsert=True)
-    return render_template("scrape.html", mars=mars)
+    # mars = mongo.db.mars.find_one()
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
