deae7aec533926c9d6185a9b33c0924e77d352e8
Duvey <rudow.david@gmail.com>
2020-11-15 12:22:59 -0600

    fixed plots

--- app.py
+++ app.py
@@ -5,12 +5,13 @@ import os
 import pymongo
 import pandas as pd
 import json
+import numpy as np
 
 import config
 
 from flask import Flask, render_template, jsonify
 from flask_pymongo import PyMongo
-# from forecast import forecastFunction
+import Forecast
 
 from pickle import load
 from sklearn.preprocessing import StandardScaler
@@ -53,20 +54,129 @@ wind_df = wind_df.drop('_id', axis=1)
 def index():
     return render_template("index.html")
 
-# @app.route("/getsolar")
-# def getSolarData():
-#     return solar_df.to_json(orient='table',index=False)
+@app.route("/getSolar")
+def getSolarData():
+    # Request parameters
+    lat = "30.238333"
+    lon = "-97.508611"
+
+    # Make API call to get weather data and turn response to JSON object
+    responseJson = Forecast.makeAPIRequest(lat, lon, config.weather_api_key)
+
+    # Get the daily weather variables from json response and store as pandas dataframe
+    daily_solar_DF = Forecast.forecasted_daily_solar(responseJson)
+
+    # Will be converting UTC time stamp for next 7 days of weather predictions
+    daily_index = np.arange(0,8,1)
+
+    # Set variable equal to "UTC_Time" column from daily_solar_DF
+    daily_UTC_date_time = daily_solar_DF["UTC_Time"]
+    # Convert UTC time to CST time
+    date_time_DF = Forecast.convert_DateTime_UTC_to_CST(daily_UTC_date_time, daily_index)
+
+    # Set variables equal to "Sunrise" and "Sunset" columns
+    UTC_sunrise = daily_solar_DF["Sunrise"]
+    UTC_sunset = daily_solar_DF["Sunset"]
+    # Calculate sunhours
+    sunhour_DF = Forecast.calculate_sunhour(UTC_sunrise, UTC_sunset, daily_index)
+
+    # Merge the weather predictions with the date/time dataframe
+    daily_weather_DF = pd.merge(daily_solar_DF, date_time_DF, on='UTC_Time', how='outer')
+    # Drop the UTC_Time column 
+    daily_weather_DF.drop(columns=["UTC_Time"], axis=1, inplace=True)
+
+    # Merge sunhours into daily weather dataframe
+    daily_weather_DF = pd.merge(daily_weather_DF, sunhour_DF, on='Sunrise', how='outer')
+    # Drop the following columns so daily weather dataframe can merge with hourly weather dataframe
+    daily_weather_DF.drop(columns=["Sunrise", "Sunset", "Date_Time", "Year", "Month", "Hour"], axis=1, inplace=True)
+
+    # Get the hourly weather variables from json response and store as pandas dataframe
+    hourly_solar_DF = Forecast.forecasted_hourly_solar(responseJson)
+
+    # Will be converting UTC timestamp for next 48 hours of weather predictions
+    hourly_index = np.arange(0,48,1)
+    # Set variable equal to "UTC_Time" column from hourly_wind_DF
+    hourly_UTC_date_time = hourly_solar_DF["UTC_Time"]
+
+    # Convert UTC time stamp to datetime, year, month, day, and hour
+    hourly_date_time_DF = Forecast.convert_DateTime_UTC_to_CST(hourly_UTC_date_time, hourly_index)
+
+    # Merge the weather predictions with the date/time dataframe
+    hourly_weather_DF = pd.merge(hourly_solar_DF, hourly_date_time_DF, on='UTC_Time', how='outer')
+    # Drop the UTC_Time column 
+    hourly_weather_DF.drop(columns=["UTC_Time"], axis=1, inplace=True)
+    # Convert "Date-Time" column to datetime object
+    hourly_weather_DF["Date_Time"] = pd.to_datetime(hourly_weather_DF["Date_Time"])
+
+    # Create the final dataframe: merge the hourly weather DF with the daily weather DF
+    forecasted_solar_DF = pd.merge(hourly_weather_DF, daily_weather_DF, on='Day', how='inner')
+
+    forecasted_solar_DF = forecasted_solar_DF[["Date_Time", "Year", "Month", "Day", "Hour", "Temperature_F", "Humidity_percent", "Sunhour", "CloudCover_percent", "uvIndex", "Weather_Description"]]
+
+    # Load the model
+    scaler = load(open('Solar/solar_ml_model/scaler.pkl', 'rb'))
+    load_nn = tf.keras.models.load_model('Solar/solar_ml_model/solar_model')
+
+    # Define the features (X) and transform the data
+    X = forecasted_solar_DF.drop(['Date_Time', 'Weather_Description','Day','Year'], axis=1)
+
+    # Transform the data
+    X_scaled = scaler.transform(X)
+
+    # Predict values for test set
+    nn_results = Forecast.modelPrediction(forecasted_solar_DF, X_scaled, load_nn)
+    return nn_results.to_json(orient='table',index=False)
+    
 
 @app.route("/getwind", methods=['POST', 'GET'])
 def getWindData():
-    return wind_df.to_json(orient='table',index=False)
+    # Request parameters
+    lat = "32.776111"
+    lon = "-99.476444"
+
+    # Make API call to get weather data and turn response to JSON object
+    responseJson = Forecast.makeAPIRequest(lat, lon, config.weather_api_key)
+    
+    # Store JSON object as pandas dataframe
+    hourly_wind_DF = Forecast.forecasted_hourly_wind(responseJson)
+    
+    # Will be converting UTC timestamp for next 48 hours of weather predictions
+    hourly_index = np.arange(0,48,1)
+    # Set variable equal to "UTC_Time" column from hourly_wind_DF
+    hourly_UTC_date_time = hourly_wind_DF["UTC_Time"]
+
+    # Convert UTC time stamp to datetime, year, month, day, and hour
+    hourly_date_time_DF = Forecast.convert_DateTime_UTC_to_CST(hourly_UTC_date_time, hourly_index)
+    
+    # Merge the weather predictions with the date/time dataframe
+    forecasted_wind_DF = pd.merge(hourly_wind_DF, hourly_date_time_DF, on='UTC_Time', how='outer')
+    # Drop the UTC_Time column 
+    forecasted_wind_DF.drop(columns=['UTC_Time'], axis=1, inplace=True)
+    # Convert "Date-Time" column to datetime object
+    forecasted_wind_DF["Date_Time"] = pd.to_datetime(forecasted_wind_DF["Date_Time"])
+    forecasted_wind_DF = forecasted_wind_DF[["Date_Time", "Year", "Month", "Day", "Hour", "Temperature_F", "Humidity_percent", "WindSpeed_mph", "WindDirection_degrees", "Weather_Description"]]
+
+    # Load the model
+    scaler = load(open('Wind/wind_ml_model/scaler.pkl', 'rb'))
+    load_nn = tf.keras.models.load_model('Wind/wind_ml_model/wind_model')
+    
+    # Define the features (X) 
+    X = forecasted_wind_DF.drop(['Date_Time', 'Weather_Description', 'Year'], axis=1)
+    # Transform the data
+    X_scaled = scaler.transform(X)
+    
+    # Make the MWH predictions using the forecasted weather dataframe and model parameters
+    nn_results = Forecast.modelPrediction(forecasted_wind_DF, X_scaled, load_nn)
+
+    return nn_results.to_json(orient='table',index=False)
 
 @app.route("/solarPredict/<YEAR>/<MONTH>/<DAY>", methods=['POST', 'GET'])
 def solarPredict(YEAR=2019, MONTH=4, DAY=20):
     MONTH = int(MONTH)
     DAY = int(DAY)
     YEAR = int(YEAR)
-    solarDayDF = solar_df.loc[(solar_df['Year'] == YEAR) & (solar_df['Month'] == MONTH) & (solar_df['Day'] == DAY)]
+    solar_df_copy = solar_df.copy()
+    solarDayDF = solar_df_copy.loc[(solar_df_copy['Year'] == YEAR) & (solar_df_copy['Month'] == MONTH) & (solar_df_copy['Day'] == DAY)]
 
     # load in the scaler
     scaler = load(open('Solar/solar_ml_model/scaler.pkl', 'rb'))
@@ -78,22 +188,42 @@ def solarPredict(YEAR=2019, MONTH=4, DAY=20):
  
     # Scaling the data.
     X_scaled = scaler.transform(X)
-    
-    # Predict values for test set
+  
+     # Make the MWH predictions using the forecasted weather dataframe and model parameters
     y_pred = load_nn.predict(X_scaled)
     y_pred = y_pred.ravel()
+
+    solarDayDF['pred'] = y_pred
+
+    return solarDayDF.to_json(orient='table',index=False)
+
+@app.route("/windPredict/<YEAR>/<MONTH>/<DAY>", methods=['POST', 'GET'])
+def windPredict(YEAR=2019, MONTH=4, DAY=20):
+    MONTH = int(MONTH)
+    DAY = int(DAY)
+    YEAR = int(YEAR)
+    windDayDF = wind_df.loc[(wind_df['Year'] == YEAR) & (wind_df['Month'] == MONTH) & (wind_df['Day'] == DAY)]
+
+    # load in the scaler
+    scaler = load(open('Wind/wind_ml_model/scaler.pkl', 'rb'))
+
+    # how to load the model
+    load_nn = tf.keras.models.load_model('Wind/wind_ml_model/wind_model')
+
+    X = windDayDF.drop(['MWH', 'WindDirection_compass', 'Year','Weather_Description', 'Date_Time','WindGust_mph'], axis=1)
+ 
+    # Scaling the data.
+    X_scaled = scaler.transform(X)
+    
+    # Make the MWH predictions using the forecasted weather dataframe and model parameters
+    y_pred = load_nn.predict(X_scaled)
+    y_pred = y_pred.ravel()
+
     # Create dataframe for results
-    nn_results = pd.DataFrame()
-    nn_results['pred'] = y_pred
-    nn_results['Hour'] = solarDayDF['Hour']
-    nn_results['Day'] = solarDayDF['Day']
-    nn_results['Date_Time'] = solarDayDF['Date_Time']
-    return nn_results.to_json(orient='table',index=False)
-    # return solarDayDF.to_json(orient='table',index=False)
+    windDayDF['pred'] = y_pred
 
+    return windDayDF.to_json(orient='table',index=False)
 
-# @app.route("/getwind/<startDate>/<endDate>", methods=['POST'])
-# def getWindData(startDate='2020-01-01', endDate='2020-01-31'):
 
 if __name__ == "__main__":
     app.run()
--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,8 @@
-
+$( "#datepicker" ).datepicker( "setDate", "04/20/2018" );
 
 $.post( "/getwind", function( data ) {
     var windData = JSON.parse(data);
 
-    var windDataTwo = windData["data"];
         var dateTime = [];
         for (var i = 0; i < windData.data.length; i ++){
             dateTime[i] = windData.data[i]["Date_Time"];
@@ -12,18 +11,18 @@ $.post( "/getwind", function( data ) {
 
         var mwh = [];
         for (var i = 0; i < windData.data.length; i ++){
-            mwh[i] = windData.data[i]["MWH"];
+            mwh[i] = windData.data[i]["pred"];
         };
         // console.log(mwh)
 
         var trace1 = {
             x: dateTime,
             y: mwh,
-            type: "bar"
+            type: "line"
         };
 
         var layout1 = {
-            title: {text: "Date vs MWH"},
+            title: {text: "Wind Prediction"},
             xaxis: {
                 title: "Date"
             },
@@ -32,20 +31,56 @@ $.post( "/getwind", function( data ) {
             }
         };
 
-    Plotly.newPlot("somePlot", [trace1], layout1);
-    console.log(windDataTwo);
+    Plotly.newPlot("windPredictPlot", [trace1], layout1);
 
     // $( "#somePlot" ).html( windDataTwo );
-})
+});
 
+$.get( "/getSolar", function( data ) {
+    
+        var solarData = JSON.parse(data);
+        console.log(solarData);
+        var dateTime = [];
+        for (var i = 0; i < solarData.data.length; i ++){
+            dateTime[i] = solarData.data[i]["Date_Time"];
+        };
+        // console.log(dateTime)
+
+        var mwh = [];
+        for (var i = 0; i < solarData.data.length; i ++){
+            mwh[i] = solarData.data[i]["pred"];
+        };
+        // console.log(mwh)
+
+        var trace1 = {
+            x: dateTime,
+            y: mwh,
+            type: "line"
+        };
+
+        var layout1 = {
+            title: {text: "Solar Prediction"},
+            xaxis: {
+                title: "Time"
+            },
+            yaxis: {
+                title: "MWH"
+            }
+        };
+
+    Plotly.newPlot("solarPredictPlot", [trace1], layout1);
+});
 
 $("#button").click(function() {
     var currentDate = $( "#datepicker" ).datepicker( "getDate" );
         day  = currentDate.getDate(),  
         month = currentDate.getMonth() + 1,              
         year =  currentDate.getFullYear();
+
     $.get( `/solarPredict/${year}/${month}/${day}`, function( data ) {
         var solarData = JSON.parse(data);
+
+        console.log(solarData);
 
         var dateTime = [];
         for (var i = 0; i < 24; i ++){
@@ -53,16 +88,28 @@ $("#button").click(function() {
         };
         console.log(dateTime);
 
-        var mwh = [];
+        var predMWH = [];
         for (var i = 0; i < solarData.data.length; i ++){
-            mwh[i] = solarData.data[i]["pred"];
+            predMWH[i] = solarData.data[i]["pred"];
         };
-        console.log(mwh);
+        console.log(predMWH);
+
+        var actualMWH = [];
+        for (var i = 0; i < solarData.data.length; i ++){
+            actualMWH[i] = solarData.data[i]["MWH"];
+        };
+        console.log(actualMWH);
 
         var trace1 = {
             x: dateTime,
-            y: mwh,
-            type: "bar"
+            y: predMWH,
+            type: "line"
+        };
+
+        var trace2 = {
+            x: dateTime,
+            y: actualMWH,
+            type: "line"
         };
 
         var layout1 = {
@@ -75,7 +122,50 @@ $("#button").click(function() {
             }
         };
 
-    Plotly.newPlot("solarHistoryPlot", [trace1], layout1);
+    Plotly.newPlot("solarHistoryPlot", [trace1, trace2], layout1);
     });
 
+    $.get( `/windPredict/${year}/${month}/${day}`, function( data ) {
+        var windData = JSON.parse(data);
+
+        var dateTime = [];
+        for (var i = 0; i < 24; i ++){
+            dateTime[i] = i;
+        };
+
+        var predMWH = [];
+        for (var i = 0; i < windData.data.length; i ++){
+            predMWH[i] = windData.data[i]["pred"];
+        };
+
+        var actualMWH = [];
+        for (var i = 0; i < windData.data.length; i ++){
+            actualMWH[i] = windData.data[i]["MWH"];
+        };
+        console.log(actualMWH);
+
+        var trace1 = {
+            x: dateTime,
+            y: predMWH,
+            type: "line"
+        };
+
+        var trace2 = {
+            x: dateTime,
+            y: actualMWH,
+            type: "line"
+        };
+
+        var layout1 = {
+            title: {text: "Date vs MWH"},
+            xaxis: {
+                title: "Date"
+            },
+            yaxis: {
+                title: "MWH"
+            }
+        };
+
+    Plotly.newPlot("windHistoryPlot", [trace1,trace2], layout1);
+    });
 });
